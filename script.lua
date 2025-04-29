bd353098f06dc37c9220e2562e4e664b481d749c
shotsdunkx <sxrcppr@gmail.com>
2025-04-29 16:34:07 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -83,13 +83,6 @@ local Button = Tab:CreateButton({
     	end
 })
 local Button = Tab:CreateButton({
-	Name = "Muscle Legends",
-	Description = nil, 
-    	Callback = function()
-      loadstring(game:HttpGet(('https://pastebin.com/raw/j3wU2wx0'),true))()
-    	end
-})
-local Button = Tab:CreateButton({
 	Name = "Bubble Gum Simulator Infinity",
 	Description = nil, 
     	Callback = function()
@@ -101,5 +94,12 @@ local Button = Tab:CreateButton({
 	Description = nil, 
     	Callback = function()
       loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    	end
+})
+local Button = Tab:CreateButton({
+	Name = "Muscle Legends",
+	Description = nil, 
+    	Callback = function()
+      loadstring(game:HttpGet(('https://pastebin.com/raw/j3wU2wx0'),true))()
     	end
 })
