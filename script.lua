b530fee77fb373c6210774d6e5d0875d8f7e0fbf
shotsdunkx <sxrcppr@gmail.com>
2025-05-06 19:31:03 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -117,3 +117,10 @@ local Button = Tab:CreateButton({
       loadstring(game:HttpGet("https://gist.githubusercontent.com/ScriptsForDays/c4b8eddc6bd3aa3c1a349e604adc5525/raw/70a02fc583deb64689932a8c1c873d6fd38afe23/Brainrot-Evo"))()
     	end
 })
+local Button = Tab:CreateButton({
+	Name = "Grow A Garden",
+	Description = nil, 
+    	Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/ameicaa1/Grow-a-Garden/main/Grow_A_Garden.lua"))()
+    	end
+})
